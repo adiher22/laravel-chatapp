bc8b8e5583821a84a61fb686ca341baa61c92dd8
Adi Hernawan <adiherna2210@bsi.ac.id>
2021-01-20 22:15:32 +0700

    Mengubah file container.vue and inputMessage

--- resources/js/Pages/Chat/messageContainer.vue
+++ resources/js/Pages/Chat/messageContainer.vue
@@ -1,9 +1,13 @@
 <template>
-    <div>Message Container</div>
+    <div class="h-96 w-full">
+        <div class="h-full p-2 flex flex-col-reverse overflow-scroll">
+
+        </div>
+    </div>
 
 </template>
 <script>
 export default {
-    
+    propss: ['messages']
 }
 </script>
